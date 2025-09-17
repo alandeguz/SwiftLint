c86bd67b8526ab9cc905c178933484d8c5296a9b
Alan DeGuzman <alandeguz@users.noreply.github.com>
2025-09-17 13:10:31 -0500

    0.61.0 delete everything except SwiftLintBuildToolPlugin

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'cocoapods'
-gem 'danger'
-gem 'base64'
-gem 'jazzy', '~> 0.15.3'
--- Tests/BuiltInRulesTests/StatementPositionRuleTests.swift
+++
@@ -1,9 +0,0 @@
-@testable import SwiftLintBuiltInRules
-import TestHelpers
-
-final class StatementPositionRuleTests: SwiftLintTestCase {
-    func testStatementPositionUncuddled() {
-        let configuration = ["statement_mode": "uncuddled_else"]
-        verifyRule(StatementPositionRule.uncuddledDescription, ruleConfiguration: configuration)
-    }
-}
